03f03355943251379207c0507e26dc2b05319f87
Mintesnot Girma <MintesnotGi@ethiopianairlines.com>
2025-02-18 10:52:10 +0300

    Add project files.

---
+++ TcpClient1/TCPClient.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using TcpClientServer;
+
+namespace TcpClient1
+{
+     class TCPClient
+    {
+
+        static void Main()
+        {
+
+            Thread serverThread = new Thread(() =>
+            {
+                TCPServer server = new TCPServer(5000);
+                server.Start();
+            });
+
+            serverThread.IsBackground = true;
+            serverThread.Start();
+
+            while (true) {
+
+
+                try
+                {
+                    Console.WriteLine("Enter command GET_TEMP/GET_STATUS/EXIT :- ");
+                    string command = Console.ReadLine();
+                    if (command.ToUpper() == "EXIT") break;
+
+                    using (TcpClient client = new TcpClient("127.0.0.1", 5000))
+                    using (NetworkStream stream = client.GetStream())
+                    {
+                        byte[] commandBytes = Encoding.ASCII.GetBytes(command);
+                        stream.Write(commandBytes, 0, commandBytes.Length);
+
+                        byte[] buffer = new byte[1024];
+                        int bytesRead = stream.Read(buffer, 0, buffer.Length);
+                        string response = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+                        Console.WriteLine("Server Response: " + response);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine("Error communicating with server: " + ex.Message);
+                }
+
+            }
+        }
+    }
+}
---
+++ TcpClient1/TCPServer.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TcpClientServer
+{
+
+
+    class TCPServer
+    {
+
+        private TcpListener server;
+
+
+        //public TCPServer()
+        //{
+                
+        //}
+        public TCPServer(int port)
+        {
+
+            try
+            {
+                server = new TcpListener(IPAddress.Any, port);
+                server.Start();
+                Console.WriteLine("Server started...");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error starting server: " + ex.Message);
+
+            }
+
+        }
+
+        public void Start()
+        {
+
+            try
+            {
+                while (true)
+                {
+                    TcpClient client = server.AcceptTcpClient();
+                    ThreadPool.QueueUserWorkItem(HandleClient, client); // use concurrency 
+
+                }
+            }
+            catch (Exception ex)
+            {
+
+                Console.WriteLine("Error accepting client: " + ex.Message);
+                
+            }
+
+        }
+
+        private void HandleClient(object obj) {
+
+
+            try
+            {
+                TcpClient client = (TcpClient)obj;
+                NetworkStream stream = client.GetStream();
+                byte[] buffer = new byte[1024];
+
+                int bytesRead = stream.Read(buffer, 0, buffer.Length);
+
+                string req = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+
+                string response = "Unknown Command";
+
+                switch (req.Trim().ToUpper())
+                {
+
+                    case "GET_TEMP":
+                        response = "Temprature:25°C";
+                        break;
+                    case "GET_STATUS":
+                        response = "Status :OK";
+                        break;
+                }
+
+                byte[] responeBytes = Encoding.ASCII.GetBytes(response);
+                stream.Write(responeBytes, 0, responeBytes.Length);
+                client.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error handling client: " + ex.Message);
+
+                throw;
+            }
+
+
+
+        }
+
+
+    }
+}
